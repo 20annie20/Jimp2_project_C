282d2f50161d5b55b045e035da29d93104c09ffa
trxp <61658276+trxp@users.noreply.github.com>
2020-03-23 18:14:03 +0100

    Moduł czytania danych z pliku

--- loadfile.c
+++ loadfile.c
@@ -3,57 +3,85 @@
 #include <errno.h>		//zmienna errno
 #include "loadfile.h"
 
+#define X game->xx 		//LICZBA kolumn - wymiar tablicy zgodny z osia X
+#define Y game->yy		//LICZBA rzedow - wymiar tablicy zgodny z osia Y
 
 id loadfile ( char* fn ){
 	FILE *in = fopen( fn, "r" );
 	if( in == NULL ){
-		perror( "Nie ma takiego pliku" );	
-		exit( EXIT_FAILURE );
+		perror("Nie ma takiego pliku");	
+		exit(EXIT_FAILURE);
 	}
 
 	id game = malloc ( sizeof (inputData_t) );
 	int n;			//liczba punktow
-	int a, b, c;		//zmienne pomocnicze przechowywujace czytane wartosci
+	int a, b, c;	//zmienne pomocnicze przechowywujace czytane wartosci
 
 	//pierwsze cztery dane niezbedne
 	if ( fscanf( in, "%d%d%d%d", &a, &b, &c, &n ) == 4 ){
+
+		if ( a < 0 || b < 0 || c < 0 ) {
+			errno = 5;
+			perror("Bledne wartosci w pliku"); 
+			exit ( EXIT_FAILURE );
+		}
+
 		game->g = a;
-		game->xx = b;
-		game->yy = c;
+		game->xx = b; //X
+		game->yy = c; //Y
 		
 		//alokowanie pamieci obu tablic
 		//game->t (lub d) to wskaznik na tablice wskaznikow wierszy
 		int i;
-		game->t = malloc( (game->xx) * sizeof(int*) );
-		game->d = malloc( (game->xx) * sizeof(int*) );
-   		for( i = 0; i < game->xx; i++){
-        		game->t[i] = malloc( game->yy * sizeof(int) );
-			game->d[i] = malloc( game->yy * sizeof(int) );
-    		}
+		game->t = malloc( X * sizeof(int*) );
+		if (game->t == NULL){
+			perror("Zabraklo pamieci"); 	//errno automatycznie przyjmuje odpowiednia wartosc
+			exit ( EXIT_FAILURE );
+		}
+		game->d = malloc( X * sizeof(int*) );
+		if (game->d == NULL){
+			perror("Zabraklo pamieci"); 
+			exit ( EXIT_FAILURE );
+		}
+
+   		for( i = 0; i < X; i++){
+        	game->t[i] = malloc( Y * sizeof(int) );
+			if (game->t[i] == NULL){
+				perror("Zabraklo pamieci"); 
+				exit ( EXIT_FAILURE );
+			}
+
+			game->d[i] = malloc( Y * sizeof(int) );
+			if (game->d == NULL){
+				perror("Zabraklo pamieci"); 
+				exit ( EXIT_FAILURE );
+			}
+
+    	}
 		
 		//zerowanie zawartosci obu tablic
 		int j;
-		for ( j = 0; j < game->yy; j++ ){
-            		for (i = 0; i < game->xx; i++ ){
-                		game->t[i][j] = 0;
+		for ( j = 0; j < Y; j++ ){
+            for (i = 0; i < X; i++ ){
+                game->t[i][j] = 0;
 				game->d[i][j] = 0;
-            		}
-       		}
+            }
+        }
 	
 		//czytanie n par wspolrzednych punktow
 		while (n--){
 			if ( fscanf( in, "%d%d", &a, &b) == 2 ){
-				if ( a <= game->xx && b <= game->yy )
+				if ( a <= X && b <= Y && a >= 0 && b >= 0)
 					game->t[a][b] = 1;
 				else {
 					errno = 5;
-					perror( "Bledne wartosci w pliku" ); 
+					perror("Bledne wartosci w pliku"); 
 					exit ( EXIT_FAILURE );
 				}
 			}
 			else {
 				errno = 5;
-				perror( "Zbyt malo danych" ); 
+				perror("Zbyt malo danych"); 
 				exit ( EXIT_FAILURE );
 			}
 		}
@@ -61,7 +89,7 @@ id loadfile ( char* fn ){
 	}
 	else {
 		errno = 5;
-		perror( "Bledna liczba danych" ); 
+		perror("Bledna liczba danych"); 
 		exit ( EXIT_FAILURE );
 	}
 
@@ -72,7 +100,7 @@ id loadfile ( char* fn ){
 
 void releasememory ( id game ){
 	int i;
-	for ( i = 0; i < game->xx; i++ ){
+	for ( i = 0; i < X; i++ ){
         free( game->t[i] );
 		free( game->d[i] );
     }
